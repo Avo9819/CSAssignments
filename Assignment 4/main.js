047d36a16bf552a7db2a6242d2afdb999ef92c27
Avo9819 <avo_alexanian@edu.aua.am>
2020-11-09 13:53:42 +0200

    Final

--- Assignment 4/main.js
+++ Assignment 4/main.js
@@ -1,14 +1,24 @@
 
-
+const reader = require("readline-sync")
 const mat = require("./MatrixCreator");
 const utilis = require("./utilis");
 
-let matA = mat.matrixCreator();
-// let matB = mat.matrixCreator();
-// let matC = matrixCreator();
-// let matA = [[1,2], [3,4],[5,6]]
-// console.log(utilis.matrixMultiplyer(matA , matB));
-// console.log(utilis.matrixSum(matA , matB));
-console.log(utilis.elementAdder(matA));
-console.log(utilis.elementAdder2(matA));
+let choice = reader.questionInt("Please choose the one want: \n 1.Matrix Multiplication \n 2.Matrix Sum \n 3.Sum of Arrays \n n = ")
 
+if(choice === 1) {
+    let matA = mat.matrixCreator();
+    let matB = mat.matrixCreator();
+    console.log(utilis.matrixMultiplyer(matA , matB));
+    
+}
+else if (choice === 2) {
+    let matA = mat.matrixCreator();
+    let matB = mat.matrixCreator();
+    console.log(utilis.matrixSum(matA , matB));
+}
+else if(choice === 3) {
+    
+    let matA = mat.matrixCreator();
+    console.log(utilis.elementAdder(matA));
+    console.log(utilis.elementAdder2(matA));
+}
